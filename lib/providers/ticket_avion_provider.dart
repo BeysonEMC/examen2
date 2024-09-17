ef84482974a14ace3824e292e10ad32f58fc688c
BeysonEMC <bemcjoker@gmail.com>
2024-09-16 22:52:09 -0600

    cuarto commit

--- lib/providers/ticket_avion_provider.dart
+++ lib/providers/ticket_avion_provider.dart
@@ -1,33 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_ticket_avion/models/ticket_avion.dart';
-import 'package:flutter/foundation.dart';
 
 class TicketAvionProvider with ChangeNotifier {
   List<TicketAvion> _ticketsAvion = [];
 
   List<TicketAvion> get ticketsAvion => _ticketsAvion;
 
+  Stream<List<TicketAvion>> get ticketsAvionStream {
+    return FirebaseFirestore.instance.collection('ticketsAvion').snapshots().map((querySnapshot) {
+      return querySnapshot.docs.map((doc) => TicketAvion.fromJson(doc.data())).toList();
+    });
+  }
+
   Future<void> leerTicketsAvion() async {
-    final querySnapshot = await FirebaseFirestore.instance.collection('ticketsAvion').get();
-    _ticketsAvion = querySnapshot.docs.map((doc) => TicketAvion.fromJson(doc.data())).toList();
-    notifyListeners();
+    try {
+      final querySnapshot = await FirebaseFirestore.instance.collection('ticketsAvion').get();
+      _ticketsAvion = querySnapshot.docs.map((doc) => TicketAvion.fromJson(doc.data())).toList();
+      notifyListeners();
+    } catch (e) {
+      print('Error leyendo ticketsAvion: $e');
+    }
   }
 
   Future<void> actualizarTicketAvion(TicketAvion ticketAvion) async {
-    await FirebaseFirestore.instance.collection('ticketsAvion').doc(ticketAvion.numeroVuelo).update(ticketAvion.toJson());
-    _ticketsAvion[_ticketsAvion.indexOf(ticketAvion)] = ticketAvion;
-    notifyListeners();
+    try {
+      await FirebaseFirestore.instance.collection('ticketsAvion').doc(ticketAvion.numeroVuelo).update(ticketAvion.toJson());
+      _ticketsAvion[_ticketsAvion.indexOf(ticketAvion)] = ticketAvion;
+      notifyListeners();
+    } catch (e) {
+      print('Error actualizando ticketAvion: $e');
+    }
   }
 
   Future<void> eliminarTicketAvion(TicketAvion ticketAvion) async {
-    await FirebaseFirestore.instance.collection('ticketsAvion').doc(ticketAvion.numeroVuelo).delete();
-    _ticketsAvion.remove(ticketAvion);
-    notifyListeners();
+    try {
+      await FirebaseFirestore.instance.collection('ticketsAvion').doc(ticketAvion.numeroVuelo).delete();
+      _ticketsAvion.remove(ticketAvion);
+      notifyListeners();
+    } catch (e) {
+      print('Error eliminando ticketAvion: $e');
+    }
   }
+
   Future<void> crearTicketAvion(TicketAvion ticketAvion) async {
-    await FirebaseFirestore.instance.collection('ticketsAvion').add(ticketAvion.toJson());
-    _ticketsAvion.add(ticketAvion);
-    notifyListeners();
+    try {
+      await FirebaseFirestore.instance.collection('ticketsAvion').add(ticketAvion.toJson());
+      _ticketsAvion.add(ticketAvion);
+      notifyListeners();
+    } catch (e) {
+      print('Error creando ticketAvion: $e');
+    }
   }
 }
--- lib/ticekt_avion_screen.dart
+++ lib/ticekt_avion_screen.dart
@@ -3,68 +3,102 @@ import 'package:provider/provider.dart';
 import 'package:flutter_ticket_avion/models/ticket_avion.dart';
 import 'package:flutter_ticket_avion/providers/ticket_avion_provider.dart';
 
-class TicketAvionScreen extends StatelessWidget {
+class TicketAvionScreen extends StatefulWidget {
+  @override
+  _TicketAvionScreenState createState() => _TicketAvionScreenState();
+}
+
+class _TicketAvionScreenState extends State<TicketAvionScreen> {
+  final _numeroVueloController = TextEditingController();
+  final _aerolineaController = TextEditingController();
+  final _informacionPasajeroController = TextEditingController();
+  final _origenController = TextEditingController();
+  final _destinoController = TextEditingController();
+  final _asientoController = TextEditingController();
+  final _claseController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => TicketAvionProvider(),
-      child: Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Ticket Avion'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Número de vuelo',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Aerolínea',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Información del pasajero',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Origen',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Destino',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Asiento',
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Clase',
-            ),
-          ),
-          ElevatedButton(
-  onPressed: () {
-    final ticketAvion = TicketAvion(
-      numeroVuelo: '12345',
-      aerolinea: 'Aerolínea 1',
-      informacionPasajero: 'Información del pasajero',
-      origen: 'Origen',
-      destino: 'Destino',
-      asiento: 'Asiento',
-      clase: 'Clase',
-    );
-    Provider.of<TicketAvionProvider>(context, listen: false).crearTicketAvion(ticketAvion);
-  },
-  child: Text('Crear ticket avion'),
-),
-          ],
+      body: ChangeNotifierProvider(
+        create: (_) => TicketAvionProvider(),
+        child: Builder(
+          builder: (context) {
+            return Column(
+              children: [
+                TextField(
+                  controller: _numeroVueloController,
+                  decoration: InputDecoration(
+                    labelText: 'Número de vuelo',
+                  ),
+                ),
+                TextField(
+                  controller: _aerolineaController,
+                  decoration: InputDecoration(
+                    labelText: 'Aerolínea',
+                  ),
+                ),
+                TextField(
+                  controller: _informacionPasajeroController,
+                  decoration: InputDecoration(
+                    labelText: 'Información del pasajero',
+                  ),
+                ),
+                TextField(
+                  controller: _origenController,
+                  decoration: InputDecoration(
+                    labelText: 'Origen',
+                  ),
+                ),
+                TextField(
+                  controller: _destinoController,
+                  decoration: InputDecoration(
+                    labelText: 'Destino',
+                  ),
+                ),
+                TextField(
+                  controller: _asientoController,
+                  decoration: InputDecoration(
+                    labelText: 'Asiento',
+                  ),
+                ),
+                TextField(
+                  controller: _claseController,
+                  decoration: InputDecoration(
+                    labelText: 'Clase',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    final ticketAvion = TicketAvion(
+                      numeroVuelo: _numeroVueloController.text,
+                      aerolinea: _aerolineaController.text,
+                      informacionPasajero: _informacionPasajeroController.text,
+                      origen: _origenController.text,
+                      destino: _destinoController.text,
+                      asiento: _asientoController.text,
+                      clase: _claseController.text,
+                    );
+                    Provider.of<TicketAvionProvider>(context, listen: false).crearTicketAvion(ticketAvion);
+                    _numeroVueloController.clear();
+                    _aerolineaController.clear();
+                    _informacionPasajeroController.clear();
+                    _origenController.clear();
+                    _destinoController.clear();
+                    _asientoController.clear();
+                    _claseController.clear();
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Ticket ingresado correctamente')),
+                    );
+                  },
+                  child: Text('Crear ticket avion'),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
